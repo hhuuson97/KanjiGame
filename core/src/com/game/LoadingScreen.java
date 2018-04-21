0974d51fa1619e7b69966db1d4cb3beec8e96814
Ho Huu Son <hhuuson97@gmail.com>
2018-04-21 10:26:49 +0700

    first commit

---
+++ core/src/com/game/LoadingScreen.java
@@ -0,0 +1,35 @@
+package com.game;
+
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
+class LoadingScreen {
+    private Stage stage;
+    private AssetManager assetManager;
+    private ProgressBar progressBar;
+
+    LoadingScreen(Stage stage, AssetManager assetManager, Skin skin) {
+        this.stage = stage;
+        this.assetManager = assetManager;
+        progressBar = new ProgressBar(0, 1, 0.01f, false, skin);
+        Table table = new Table();
+        table.setFillParent(true);
+        table.add(new Label("Please waiting ...", skin)).pad(5).row();
+        table.add(progressBar).growX().padLeft(50).padRight(50);
+        stage.addActor(table);
+    }
+
+    void render() {
+        float percent = assetManager.getProgress();
+
+        progressBar.setValue(percent);
+    }
+
+    void hide() {
+        progressBar.setDisabled(true);
+    }
+}

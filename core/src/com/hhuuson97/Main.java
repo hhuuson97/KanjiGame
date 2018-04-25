8a5fa4f5aeb23a53ac74d76b5ea64ffda7a420e3
Ho Huu Son <hhuuson97@gmail.com>
2018-04-25 17:36:20 +0700

    pub ver

---
+++ core/src/com/hhuuson97/Main.java
@@ -0,0 +1,104 @@
+package com.hhuuson97;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+
+public class Main extends ApplicationAdapter {
+	private static final String[] gradeName = {"firstGrade", "secondGrade", "thirdGrade", "fourthGrade", "fifthGrade", "sixthGrade"};
+
+	private SpriteBatch batch;
+	BitmapFont simsunFont;
+	BitmapFont simsunFontSmall;
+	private BitmapFont bitmapFont;
+	private Stage stage;
+	private AssetManager assetManager;
+	private Skin glassy;
+
+	int status;
+	private String radicals;
+	private String[] kanjiGrade;
+	String[] kunGrade;
+	String hiragana;
+	private boolean loadingDone;
+
+	private LoadingScreen loadingScreen;
+	WellcomeScreen wellcomeScreen;
+	JukugoJumble jukugoJumble;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		stage = new Stage();
+		stage.setActionsRequestRendering(true);
+		Gdx.input.setInputProcessor(stage);
+
+		batch = new SpriteBatch();
+		bitmapFont = new BitmapFont();
+
+		radicals = Gdx.files.internal("radicals.txt").readString();
+		hiragana = Gdx.files.internal("hiragana.txt").readString();
+		kanjiGrade = new String[gradeName.length];
+		kunGrade = new String[gradeName.length];
+		for (int i = 0; i < gradeName.length; i++) {
+			kanjiGrade[i] = Gdx.files.internal(gradeName[i] + "/kanji.txt").readString();
+			kunGrade[i] = Gdx.files.internal(gradeName[i] + "/kun.txt").readString();
+		}
+
+		assetManager = new AssetManager();
+		assetManager.load("simsun/simsun.fnt", BitmapFont.class);
+		assetManager.load("sgx/sgx-ui.json", Skin.class);
+		assetManager.load("golden-ui/golden-ui-skin.json", Skin.class);
+		glassy = new Skin(Gdx.files.internal("glassy/glassy-ui.json"));
+
+		loadingScreen = new LoadingScreen(stage, assetManager, glassy);
+		loadingDone = false;
+	}
+
+	private void onAssetUpdate() {
+		simsunFontSmall = assetManager.get("simsun/simsun.fnt", BitmapFont.class);
+		simsunFont = new BitmapFont(simsunFontSmall.getData().fontFile);
+		simsunFont.getData().setScale(2);
+		Skin sgx = assetManager.get("sgx/sgx-ui.json", Skin.class);
+		Skin golden = assetManager.get("golden-ui/golden-ui-skin.json", Skin.class);
+		wellcomeScreen = new WellcomeScreen(stage, glassy, this);
+		wellcomeScreen.menuScreen();
+		jukugoJumble = new JukugoJumble(stage, glassy, sgx, golden, this);
+		loadingScreen.hide();
+		loadingDone = true;
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1f, 1f, 1f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		if (assetManager.update() && !loadingDone) onAssetUpdate();
+		VectorGradient.getInstance().rect(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), Color.WHITE, Color.NAVY);
+		if (!loadingDone) loadingScreen.render();
+		stage.act();
+		stage.draw();
+		batch.end();
+	}
+
+	@Override
+	public void resize (int width, int height) {
+		stage.getViewport().update(width, height, true);
+	}
+
+	@Override
+	public void dispose () {
+		assetManager.dispose();
+		batch.dispose();
+		simsunFont.dispose();
+		bitmapFont.dispose();
+		stage.dispose();
+	}
+}
---
+++ core/src/com/hhuuson97/WellcomeScreen.java
@@ -0,0 +1,90 @@
+package com.hhuuson97;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+
+class WellcomeScreen {
+
+    private Stage stage;
+    private Skin skin;
+    private Table tableMenu;
+    private Table tableSelectGame;
+
+    private Main main;
+
+    WellcomeScreen(Stage stage, Skin skin, Main main) {
+        this.stage = stage;
+        this.main = main;
+        this.skin = skin;
+    }
+
+    private void gameTitle(Table table) {
+        Label label = new Label("Jukogo Jumble", skin.get("big", Label.LabelStyle.class));
+        table.add(label).padBottom(60);
+        table.row();
+    }
+
+    private void createButton(String text, Skin skin, Table table, ChangeListener changeListener) {
+        TextButton btn = new TextButton(text, skin);
+        btn.addListener(changeListener);
+        table.add(btn).padBottom(30);
+        table.row();
+    }
+
+    private Table createTable(Stage stage) {
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+        return table;
+    }
+
+    void menuScreen() {
+        stage.clear();
+        tableMenu = createTable(stage);
+        gameTitle(tableMenu);
+        createButton("Start", skin, tableMenu, new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                main.jukugoJumble.selectScreen();
+            }
+        });
+        createButton("About", skin, tableMenu, new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+
+            }
+        });
+        createButton("Exit", skin, tableMenu, new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
+        main.status = 0;
+    }
+
+//    private void selectScreen() {
+//        stage.clear();
+//        tableSelectGame = createTable(stage);
+//        gameTitle(tableSelectGame);
+//        createButton("Jukugo", skin, tableSelectGame, new ChangeListener() {
+//            @Override
+//            public void changed(ChangeEvent event, Actor actor) {
+//                main.jukugoJumble.selectScreen();
+//            }
+//        });
+//        createButton("Back", skin, tableSelectGame, new ChangeListener() {
+//            @Override
+//            public void changed(ChangeEvent event, Actor actor) {
+//                menuScreen();
+//            }
+//        });
+//        main.status = 1;
+//    }
+}
